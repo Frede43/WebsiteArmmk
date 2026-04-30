6bd88a493e40b39516e7f08588f056369caf8b07
Frede43 <alainasifiwe4242@gmail.com>
2026-05-01 01:08:49 +0200

    Correction de l'erreur 500 sur les images

--- middleware.ts
+++ middleware.ts
@@ -43,6 +43,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|files).*)'],
+  // Matcher ignoring `/_next/`, `/api/`, and static images
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 }
